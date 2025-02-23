652755a234f1612053401f85b48675485910b7e7
eraDong <qq1184434988@gmail.com>
2025-02-23 15:42:33 +0800

    try to configure connpool

--- repo/main_test.go
+++ repo/main_test.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/eraDong/NanaChat/bootstrap"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -18,7 +19,18 @@ func TestMain(m *testing.M) {
 		log.Fatal("cannot load env, err:", err)
 	}
 
-	connPool, err := pgxpool.New(context.Background(), cfg.DBSource.DSN())
+	poolConfig, err := pgxpool.ParseConfig(cfg.DBSource.DSN())
+	if err != nil {
+		log.Fatal("parse config failed:", err)
+	}
+
+	poolConfig.MaxConns = 20
+	poolConfig.MinConns = 5
+	poolConfig.MaxConnLifetime = 30 * time.Minute
+	poolConfig.MaxConnIdleTime = 5 * time.Minute
+
+	connPool, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
+	// 设置连接池参数
 
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
